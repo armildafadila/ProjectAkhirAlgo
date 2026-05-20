40d987eebbc10a5c27e82b12a01bedc979d4f0f0
Armilda <armildafadila@gmail.com>
2026-05-20 10:39:23 +0700

    yuhu

--- rental.cpp
+++ rental.cpp
@@ -17,3 +17,23 @@ struct motor
 };
 
 motor *head = NULL;
+
+//void simpanfile
+void simpanFile(){
+    FILE *fp = fopen("rentalMotor.txt", "w");
+
+    motor* bantu = head;
+    while (bantu != NULL) {
+        fprintf(fp, "%s;%s;%d;%d;%s;%s;%d\n",
+            bantu->namaMtr,
+            bantu->plat,
+            bantu->tahun,
+            bantu->harga,
+            bantu->status,
+            bantu->penyewa,
+            bantu->total);
+
+        bantu = bantu->next; // lanjut ke node berikutnya 
+    }
+    fclose(fp);
+}
