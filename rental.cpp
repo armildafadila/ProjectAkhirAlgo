f0c91f61b9556e86aec2235b03080ce9dc5312e1
Armilda <armildafadila@gmail.com>
2026-05-20 10:38:47 +0700

    uu

--- rental.cpp
+++ rental.cpp
@@ -4,3 +4,16 @@
 #include <cctype>
 using namespace std;
 
+struct motor
+{
+    char namaMtr[50];
+    char plat[15];
+    int tahun;
+    int harga;
+    char status[20];
+    char penyewa[60];
+    int total;
+    motor *next;
+};
+
+motor *head = NULL;
